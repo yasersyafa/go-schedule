97eb236c11bc221b99f7ffe2aceeeaec6e908342
Yaser Syafa <syafayaser@gmail.com>
2026-09-06 02:57:02 +0700

    feat(activity): add model activity

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.25.1
 require github.com/joho/godotenv v1.5.1
 
 require (
+	github.com/google/uuid v1.6.0
 	github.com/jmoiron/sqlx v1.4.0 // indirect
 	github.com/lib/pq v1.12.3
 )
---
+++ internal/activity/model.go
@@ -0,0 +1,19 @@
+package activity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Activity struct {
+	ID uuid.UUID `db:"id"`
+	Name string `db:"name"`
+	Notes *string `db:"notes"`
+	Day string `db:"day"`
+	StartTime string `db:"start_time"`
+	EndTime string `db:"end_time"`
+	LastNotifiedDate *time.Time `db:"last_notified_date"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
+}
