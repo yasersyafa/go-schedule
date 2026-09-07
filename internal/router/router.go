227349858a5da2a3fd29a4d896215bb169b46430
Yaser Syafa <syafayaser@gmail.com>
2026-09-07 14:40:00 +0700

    feat(cors): update CORS configuration to allow requests from localhost:5173

--- internal/router/router.go
+++ internal/router/router.go
@@ -11,7 +11,7 @@ func New(activityHandler *activity.Handler) *gin.Engine {
 
 	// CORS setup
 	r.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"http://localhost:3000"},
+		AllowOrigins: []string{"http://localhost:3000", "https://localhost:5173"},
 		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
 		AllowHeaders: []string{"Content-Type"},
 		AllowCredentials: false,
