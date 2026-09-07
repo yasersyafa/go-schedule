b1b573599bbe59cc254aa40ca666b7d9efe9141c
Yaser Syafa <syafayaser@gmail.com>
2026-09-07 13:49:47 +0700

    feat(cors): add CORS configuration to router

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,12 +1,21 @@
 package router
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/yasersyafa/go-schedule/internal/activity"
 )
 
 func New(activityHandler *activity.Handler) *gin.Engine {
 	r := gin.Default()
+
+	// CORS setup
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+		AllowHeaders: []string{"Content-Type"},
+		AllowCredentials: false,
+	}))
 
 	api := r.Group("/api/v1")
 	{
